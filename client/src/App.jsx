8cec98177f46631361a0dd67928702f0c7fb9a81
Akash Behara <akashbehara33@gmail.com>
2022-02-28 19:07:43 +0530

    initail commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const App = () => {
+  return (
+    <div className="bg-red-100">
+      <h1 className="text-3xl  mt-11">Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
